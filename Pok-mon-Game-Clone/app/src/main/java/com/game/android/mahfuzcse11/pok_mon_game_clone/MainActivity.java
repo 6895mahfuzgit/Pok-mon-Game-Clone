9e3431caedbd76dbbda881e7062a51277d5eff2a
6895mahfuz@gmail.com <6895mahfuz@gmail.com>
2017-06-25 19:50:57 +0600

    initial project created

---
+++ Pok-mon-Game-Clone/app/src/main/java/com/game/android/mahfuzcse11/pok_mon_game_clone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.game.android.mahfuzcse11.pok_mon_game_clone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
